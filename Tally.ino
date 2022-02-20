b18bf064b4e05063fc928df00ed70f3f668ac9af
SoundFreak97 <timo.winkler97@gmail.com>
2022-02-20 13:50:17 +0100

    Update Tally.ino

--- Tally.ino
+++ Tally.ino
@@ -1,5 +1,4 @@
 // http://librarymanager/All#M5StickC https://github.com/m5stack/M5StickC
-#include <M5StickC.h>
 #include <WiFi.h>
 
 // Download these from here
@@ -32,11 +31,6 @@ const char* password = "";
 
 ATEMstd AtemSwitcher;
 
-int orientation = 0;
-int orientationPrevious = 0;
-int orientationMillisPrevious = millis();
-int buttonBMillis = 0;
-
 int cameraNumber = 1;
 
 int previewTallyPrevious = 1;
@@ -53,11 +47,6 @@ void setup() {
   }
   Serial.println("Connected to the WiFi network");
 
-  // 初期化
-  M5.begin();
-  M5.MPU6886.Init();
-  M5.Lcd.setRotation(orientation);
-
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
 
@@ -65,66 +54,15 @@ void setup() {
   AtemSwitcher.serialOutput(0x80);
   AtemSwitcher.connect();
 
-  // GPIO初期化
-  pinMode(26, INPUT); // PIN  (INPUT, OUTPUT, ANALOG)無線利用時にはANALOG利用不可, DAC出力可
-  pinMode(36, INPUT); // PIN  (INPUT,       , ANALOG)入力専用、INPUT_PULLUP等も不可
-  pinMode( 0, INPUT); // PIN  (INPUT, OUTPUT,       )外部回路でプルアップ済み
-  pinMode(32, INPUT); // GROVE(INPUT, OUTPUT, ANALOG)
-  pinMode(33, INPUT); // GROVE(INPUT, OUTPUT, ANALOG)
+  // GPIO
+  pinMode(26, INPUT); 
+  pinMode(36, INPUT);
+  pinMode( 0, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
 }
-
-void setOrientation() {
-  float accX = 0, accY = 0, accZ = 0;
-  M5.MPU6886.getAccelData(&accX, &accY, &accZ);
-  //Serial.printf("%.2f   %.2f   %.2f \n",accX * 1000, accY * 1000, accZ * 1000);
-
-  if (accZ < .9) {
-    if (accX > .6) {
-      orientation = 1;
-    } else if (accX < .4 && accX > -.5) {
-      if (accY > 0) {
-        orientation = 0;
-      } else {
-        orientation = 2;
-      }
-    } else {
-      orientation = 3;
-    }
-  }
-
-  if (orientation != orientationPrevious) {
-    Serial.printf("Orientation changed to %d\n", orientation);
-    M5.Lcd.setRotation(orientation);
-  }
-}
-
 
 void loop() {
-  // ボタンの状態更新
-  M5.update();
-
-  if (AUTOUPDATE_ORIENTATION) {
-    if (orientationMillisPrevious + 500 < millis()) {
-      setOrientation();
-      orientationMillisPrevious = millis();
-    }
-  }
-
-  if (M5.BtnA.wasPressed()) {
-    AtemSwitcher.changeProgramInput(cameraNumber);
-  }
-  if (M5.BtnB.wasPressed()) {
-    setOrientation();
-    buttonBMillis = millis();
-  }
-
-  if (M5.BtnB.isPressed() && buttonBMillis != 0 && buttonBMillis < millis() - 500) {
-    Serial.println("Changing camera number");
-    cameraNumber = (cameraNumber % 4) + 1;
-    Serial.printf("New camera number: %d\n", cameraNumber);
-
-    buttonBMillis = 0;
-  }
 
   // Check for packets, respond to them etc. Keeping the connection alive!
   AtemSwitcher.runLoop();
@@ -132,7 +70,7 @@ void loop() {
   int programTally = AtemSwitcher.getProgramTally(cameraNumber);
   int previewTally = AtemSwitcher.getPreviewTally(cameraNumber);
 
-  if ((orientation != orientationPrevious) || (cameraNumber != cameraNumberPrevious) || (programTallyPrevious != programTally) || (previewTallyPrevious != previewTally)) { // changed?
+  if ((programTallyPrevious != programTally) || (previewTallyPrevious != previewTally)) { // changed?
     if (programTally && !previewTally) { // only program
       drawLabel(RED, BLACK, LOW);
     } else if (programTally && previewTally) { // program AND preview
@@ -152,15 +90,6 @@ void loop() {
 
 void drawLabel(unsigned long int screenColor, unsigned long int labelColor, bool ledValue) {
   digitalWrite(LED_PIN, ledValue);
-  M5.Lcd.fillScreen(screenColor);
-  M5.Lcd.setTextColor(labelColor, screenColor);
-  drawStringInCenter(String(cameraNumber), 8);
 }
 
-void drawStringInCenter(String input, int font) {
-  int datumPrevious = M5.Lcd.getTextDatum();
-  M5.Lcd.setTextDatum(MC_DATUM);
-  M5.Lcd.drawString(input, M5.Lcd.width() / 2, M5.Lcd.height() / 2, font);
-  M5.Lcd.setTextDatum(datumPrevious);
-}
 
